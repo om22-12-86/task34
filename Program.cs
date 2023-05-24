3020fc05a7c31d5b88d8a25b140daa8cff76bb01
om <130356845+om22-12-86@users.noreply.github.com>
2023-05-24 21:30:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+void RandomArr(int[] arr)
+ {
+    for (int i = 0; i < arr.Length; i++)
+    {
+     arr[i] = new Random().Next(100, 999);
+    }
+ }
+void PrintArray(int[] arr)
+ {
+    for (int i = 0; i < arr.Length; i++)
+    {
+     Console.Write($"{arr[i]} ");
+    }
+ }
+void CountEvenNumbers(int[] arr)
+ { 
+  int count = 0;
+  for (int i = 0; i < arr.Length; i = i + 2)
+  {
+    count = count + 1;
+  }
+  Console.WriteLine(count);
+ }
+int[] array = new int[7];
+RandomArr(array);
+PrintArray(array);
+Console.WriteLine();
+CountEvenNumbers(array);
